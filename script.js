2eceb0327e3c3fc868d0347f440d92d4e8d5af9c
VISHAL KUMAR <vishaltalreja01@gmail.com>
2025-08-09 13:15:04 +0500

    JS script to switch pricing plan

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ document.addEventListener("DOMContentLoaded", function () {
     closeIcon.classList.toggle("hidden");
   });
 });
+
+// Toggle Pricing Plan
+const monthlyBtn = document.getElementById("monthly-btn");
+const annualBtn = document.getElementById("annual-btn");
+const prices = document.querySelectorAll(".price");
+
+monthlyBtn.addEventListener("click", () => {
+  prices.forEach((price) => {
+    price.innerHTML = `$${price.dataset.monthly} <span class="text-sm text-gray-500 font-normal">/month</span>`;
+  });
+  monthlyBtn.classList.add("bg-[#d87800]", "text-white");
+  annualBtn.classList.remove("bg-[#d87800]", "text-white");
+  annualBtn.classList.add("text-gray-400");
+});
+
+annualBtn.addEventListener("click", () => {
+  prices.forEach((price) => {
+    price.innerHTML = `$${price.dataset.annual} <span class="text-sm text-gray-500 font-normal">/month</span>`;
+  });
+  annualBtn.classList.add("bg-[#d87800]", "text-white");
+  monthlyBtn.classList.remove("bg-[#d87800]", "text-white");
+  monthlyBtn.classList.add("text-gray-400");
+});
